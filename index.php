d2f2e4e22f47ef46b8c98784bc553abbc782a3cc
Nikil Viswanathan <nikil@stanford.edu>
2012-11-03 04:57:25 -0700

    Google Analytics

--- index.php
+++ index.php
@@ -13,6 +13,21 @@
   </head>
   <body>
  	
+  <!-- GA-->
+  <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36059984-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  </script>
+
  	<!-- Part 1: Wrap all page content here -->
     <div id="wrap">
 
